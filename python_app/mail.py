b6289306ed42aea5a4f85e98eca867403ba360da
Lagbalu Lateef <chelseg14@gmail.com>
2023-04-24 10:34:22 +0100

    first commit

---
+++ python_app/mail.py
@@ -0,0 +1,29 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'deevoetest@gmail.com'
+email_password = 'boehltmpvbyvohot'
+
+
+email_receiver = 'toyiteh243@ippals.com'
+
+subject = 'Learning Python'
+body  = '''
+
+Hello Deevoe Lee, 😎
+
+https://temp-mail.io/en
+'''
+
+em = EmailMessage()
+em["From"] = email_sender
+em['To'] = email_sender
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
